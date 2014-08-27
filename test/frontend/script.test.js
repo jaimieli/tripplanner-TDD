174dccde28cd8969820d26b5a7d8d1de8ada3d15
Gabriel Lebec <glebec@gmail.com>
2014-08-27 17:53:55 -0400

    refactored karma to use a sinon stub instead of jquery

--- test/frontend/script.test.js
+++ test/frontend/script.test.js
@@ -1,18 +1,20 @@
 var expect = chai.expect;
 
-describe('Karma runner setup', function () {
-    it('verifies basic logic', function (done) {
-        expect(1+1).to.equal(2);
-        done();
-    });
+describe('Karma runner setup', function() {
+  it('verifies basic logic', function(done) {
+    expect(1 + 1).to.equal(2);
+    done();
+  });
 });
 
-describe('Trip Array', function () {
-  it('should start as an empty array', function (done) {
+describe('Trip Array', function() {
+  it('should start as an empty array', function(done) {
     expect(trip).to.eql([]);
     done();
   });
   it('should have a day object after addNewDay', function(done) {
+    $ = sinon.stub();
+    $.returns(sinon.stub({click: function(){}, append: function(){}}));
     addNewDay();
     expect(trip.length).to.equal(1);
     done();
