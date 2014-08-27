883df02147a2d8106ad439790cc393e34cb6b5e3
Gabriel Lebec <glebec@gmail.com>
2014-08-27 17:33:24 -0400

    initialized working karma test module

---
+++ test/frontend/script.test.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+
+describe('Karma runner setup', function () {
+    it('verifies basic logic', function (done) {
+        expect(1+1).to.equal(2);
+        done();
+    });
+});
